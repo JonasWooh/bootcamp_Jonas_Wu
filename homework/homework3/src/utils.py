aa597fae3db896bcc37a421eaa7555358b36a8ed
JonasWyn <1061884512@qq.com>
2025-08-15 09:28:06 -0400

    stage03: replace the dataset used since did not find certain dataset yesterday

--- homework/homework3/src/utils.py
+++ homework/homework3/src/utils.py
@@ -1,13 +1,4 @@
 import pandas as pd
-import os
-
-file_path = '../data/test_data.csv'
-
-df = pd.read_csv(file_path)
-
-df.info()
-
-df.head()
 
 def get_summary_stats(dataframe):
     # 1. Get descriptive statistics
@@ -15,8 +6,7 @@ def get_summary_stats(dataframe):
     
     # 2. Get groupby aggregation
     category_summary = dataframe.groupby('category').agg(
-        mean_price=('price', 'mean'),
-        total_quantity_sold=('quantity_sold', 'sum'),
-        average_rating=('rating', 'mean')).reset_index()
+        mean_value=('value', 'mean'),
+        sum_value=('value', 'sum')).reset_index()
     
     return numeric_summary, category_summary
